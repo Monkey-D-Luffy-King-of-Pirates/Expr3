1cd2fb038b589b85948f8938746b47ffaacce358
Демон Чайный <volshebnuyoleg666@yandex.ru>
2023-07-11 18:08:33 +0400

    Добавьте файлы проекта.

---
+++ Expr3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 19 % 12;
+            a = a * 30 - ((a - 6) * (a / 6 * 60));
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
